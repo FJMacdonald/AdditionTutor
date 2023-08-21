64a299312077f27597221ee3789d8a33ced9500c
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-21 16:38:46 -0400

    Initial Commit

---
+++ AdditionTutor/AdditionTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdditionTutorApp.swift
+//  AdditionTutor
+//
+//  Created by Francesca MACDONALD on 2023-08-21.
+//
+
+import SwiftUI
+
+@main
+struct AdditionTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
